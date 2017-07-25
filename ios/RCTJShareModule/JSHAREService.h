356c6c7dc77381c5dd62f67b76efa76b54f1ee7d
huangminlinux <380108184@qq.com>
2017-07-25 17:01:51 +0800

    create ios project

---
+++ ios/RCTJShareModule/JSHAREService.h
@@ -0,0 +1,424 @@
+/*
+ *	| |    | |  \ \  / /  | |    | |   / _______|
+ *	| |____| |   \ \/ /   | |____| |  / /
+ *	| |____| |    \  /    | |____| |  | |   _____
+ * 	| |    | |    /  \    | |    | |  | |  |____ |
+ *  | |    | |   / /\ \   | |    | |  \ \______| |
+ *  | |    | |  /_/  \_\  | |    | |   \_________|
+ *
+ * Copyright (c) 2011 ~ 2017 Shenzhen HXHG. All rights reserved.
+ */
+
+
+#define JSHARE_VERSION_NUMBER 1.2.0
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, JSHAREPlatform) {
+    JSHAREPlatformWechatSession = 1,
+    JSHAREPlatformWechatTimeLine = 2,
+    JSHAREPlatformWechatFavourite = 3,
+    
+    JSHAREPlatformQQ = 4,
+    JSHAREPlatformQzone = 5,
+    
+    JSHAREPlatformSinaWeibo = 6,
+    JSHAREPlatformSinaWeiboContact = 7,
+};
+
+typedef NS_ENUM(NSUInteger,JSHAREState){
+    JSHAREStateSuccess = 1,
+    JSHAREStateFail = 2,
+    JSHAREStateCancel = 3,
+    JSHAREStateUnknown = 4,
+};
+
+typedef NS_ENUM(NSUInteger,JSHAREMediaType){
+    JSHAREText = 1,
+    JSHAREImage = 2,
+    JSHARELink = 3,
+    JSHAREAudio = 4,
+    JSHAREVideo = 5,
+    JSHAREApp = 6,
+    JSHAREFile = 7,
+    JSHAREEmoticon = 8,
+    JSHAREUndefined = 100,
+};
+
+@class JSHARESocialUserInfo;
+typedef void(^JSHAREStateHandler)(JSHAREState state,NSError *error);
+typedef void(^JSHARESocialHandler)(JSHARESocialUserInfo *userInfo,NSError *error);
+
+@interface JSHARELaunchConfig : NSObject
+
+/**
+ appKey 一个 JIGUANG 应用必须的,唯一的标识. 请参考 JIGUANG 相关说明文档来获取这个标识。
+ */
+@property (nonatomic, copy) NSString *appKey;
+
+/**
+ channel 发布渠道. 可选。
+ */
+@property (nonatomic, copy) NSString *channel;
+
+/**
+ advertisingIdentifier 广告标识符（IDFA). 可选，IDFA能帮助您更准确的统计。
+ */
+@property (nonatomic, copy) NSString *advertisingId;
+
+/**
+ isProduction 是否生产环境. 如果为开发状态,设置为NO; 如果为生产状态,应改为 YES.默认为NO。
+ */
+@property (nonatomic, assign) BOOL isProduction;
+
+/**
+ 设置微信的应用标识。
+ */
+@property (nonatomic, copy) NSString *WeChatAppId;
+
+/**
+ 设置微信的应用密匙。
+ */
+@property (nonatomic, copy) NSString *WeChatAppSecret;
+
+/**
+ 设置QQ应用标识。
+ */
+@property (nonatomic, copy) NSString *QQAppId;
+
+/**
+ 设置QQ应用Key。
+ */
+@property (nonatomic, copy) NSString *QQAppKey;
+
+/**
+ 设置新浪微博应用标识。
+ */
+@property (nonatomic, copy) NSString *SinaWeiboAppKey;
+
+/**
+ 设置新浪微博应用密匙。
+ */
+@property (nonatomic, copy) NSString *SinaWeiboAppSecret;
+
+/**
+ 设置新浪微博应用回调地址。
+ */
+@property (nonatomic, copy) NSString *SinaRedirectUri;
+
+/**
+ 不存在新浪客户端的情况下，是否支持新浪网页版分享，默认不支持值为NO，若需支持将此值设置为YES，具体参考官方文档。
+ */
+@property (nonatomic, assign) BOOL isSupportWebSina;
+
+@end
+
+@interface JSHAREMessage : NSObject
+
+/**
+ 标题：长度每个平台的限制而不同。
+ 微信好友：最大 512 字符。
+ 微信朋友圈：最大 512 字符。
+ 微信收藏：最大 512 字符。
+ QQ：最大 128 字符。
+ QQ空间：最大 128 字符。
+ 新浪微博：分享链接类型，最大 1 K字符。
+ */
+@property (nonatomic,strong) NSString *title;
+
+/**
+ 文本：文本内容，长度每个平台的限制而不同。
+ 在分享非文本类型时，此字段作为分享内容的描述使用。
+ 
+ 微信好友：分享文本类型时，最大 10 K字符。分享非文本类型，最大 1 K字符。
+ 微信朋友圈：分享文本类型时，最大 10 K字符。分享非文本类型，最大 1 K字符。
+ 微信收藏：分享文本类型时，最大 10 K字符。分享非文本类型，最大 1 K字符。
+ QQ：分享文本类型时，最大 1536 字符。分享非文本类型，最大 512 字符。
+ QQ空间：分享文本类型时，最大 128 字符。分享非文本类型，最大 512 字符。
+ 新浪微博：最大 140 汉字。
+ 
+ */
+@property (nonatomic,strong) NSString *text;
+
+/**
+ 链接：根据媒体类型填入链接，长度每个平台的限制不同。分享非文本及非图片类型时，必要！
+ 微信好友：最大 10 K字符。
+ 微信朋友圈：最大 10 K字符。
+ 微信收藏：最大 10 K字符。
+ QQ：最大 512 字符。
+ QQ空间：最大 512 字符。
+ 新浪微博：最大 512 字符。
+ 
+ */
+@property (nonatomic,strong) NSString *url;
+
+
+/**
+ 本地视频AssetURL:分享本地视频到 QQ 空间的必填参数，可传ALAsset的ALAssetPropertyAssetURL，或者PHAsset的localIdentifier。
+ */
+@property (nonatomic,strong) NSString *videoAssetURL;
+
+/**
+ 缩略图：大小限制根据平台不同而不同。
+ 微信好友：最大 32 K。
+ 微信朋友圈：最大 32 K。
+ 微信收藏：最大 32 K。
+ QQ：最大 1 M。
+ QQ空间：最大 1 M。
+ 新浪微博：最大 32 K。
+ */
+@property (nonatomic,strong) NSData *thumbnail;
+
+/**
+ 图片：分享JSHAREImage类型，大小限制根据平台不同而不同，当分享JSHARELink类型时没有提供缩略图时，若此参数不为空，JSHARE将会裁剪此参数提供的图片去适配缩略图。
+ 微信好友：最大 10 M。
+ 微信朋友圈：最大 10 M。
+ 微信收藏：最大 10 M。
+ QQ：最大 5 M。
+ QQ空间：最大 5 M。
+ 新浪微博：最大 10 M。
+
+ */
+@property (nonatomic,strong) NSData *image;
+
+/**
+ 图片数组：分享到 QQ 空间支持多张图片，图片数组的元素需要为 NSData 类型，图片数量限制为20张。若只分享单张图片至 QQ 空间使用 image 字段即可。
+ */
+@property (nonatomic,strong) NSArray *images;
+
+/**
+ 分享的媒体类型。必要！
+ */
+@property (nonatomic,assign) JSHAREMediaType mediaType;
+
+/**
+ 分享的目标平台。必要！
+ */
+@property (nonatomic,assign) JSHAREPlatform platform;
+
+/**
+ 分享JSHAREAudio类型至微信平台或QQ平台时，音乐数据url地址。
+ 微信好友：最大 10 K字符。
+ 微信朋友圈：最大 10 K字符。
+ 微信收藏：最大 10 K字符。
+ QQ：最大 512 字符。
+ QQ空间：最大 512 字符。
+ 新浪微博：最大 512 字符。
+ */
+@property (nonatomic,strong) NSString *mediaDataUrl;
+
+/**
+ 分享JSHAREApp类型至微信平台时，第三方程序自定义的简单数据。
+ */
+@property (nonatomic,strong) NSString *extInfo;
+
+/**
+ 分享JSHAREFile类型或者JSHAREApp类型至微信平台时，对应的File数据以及App数据，最大 10 M。
+ */
+@property (nonatomic,strong) NSData *fileData;
+
+/**
+ 分享JSHAREFile类型至微信平台时，对应的文件后缀名，分享文件必填，否则会导致分享到微信平台出现不一致的文件类型,最大 64 字符。
+ */
+@property (nonatomic,strong) NSString *fileExt;
+
+/**
+ 分享JSHAREEmoticon类型至微信平台时，对应的表情数据，最大 10 M。
+ */
+@property (nonatomic,strong) NSData *emoticonData;
+
+/**
+ 分享至新浪微博平台时，分享参数的一个标识符，默认为 “objectId”。最大 255 字符。
+ */
+@property (nonatomic,strong) NSString *sinaObjectID;
+
+/**
+ 返回一个 JShareMessage 实例
+ 
+ @return 返回一个 JShareMessage 实例
+ */
++ (JSHAREMessage *)message;
+@end
+
+
+@interface JSHAREService : NSObject
+
+/**
+ 启动SDK,必要!
+ 
+ @param config SDK启动参数模型，不可为nil。
+ */
++ (void)setupWithConfig:(JSHARELaunchConfig *)config;
+
+
+/**
+ 处理平台回调,必要！
+ 
+ @param url 回调的url，在 Appdelegate 的 application:handleOpenURL: 中调用。不调用此接口 JSHARE 将无法提供分享回调。
+ @return 处理结果 YES为处理成功，NO为不处理。
+ */
++ (BOOL)handleOpenUrl:(NSURL *)url;
+
+/**
+ 分享
+ 
+ @param message  分享参数
+ @param handler  分享之后的回调
+ */
++ (void)share:(JSHAREMessage *)message
+      handler:(JSHAREStateHandler)handler;
+
+
+/**
+ 获取社交平台用户信息
+
+ @param platform 社交平台参数，JSHAREPlatformWechatSession表示微信平台，JSHAREPlatformQQ表示 QQ 平台，JSHAREPlatformSinaWeibo，表示新浪微博平台。
+ @param handler 获取社交平台用户信息的回调
+ */
++ (void)getSoicalUserInfo:(JSHAREPlatform)platform
+                  handler:(JSHARESocialHandler)handler;
+
+
+/**
+ 检查用户授权之后信息是否过期。
+
+ @param platform 社交平台。
+ @return 结果，YES 表示该社交平台已授权。NO 表示未授权或授权信息已过期。
+ */
++ (BOOL)isPlatformAuth:(JSHAREPlatform)platform;
+
+
+/**
+ 删除用户授权本地数据
+
+ @param platfrom 社交平台。
+ @return 取消结果，YES 表示已取消。
+ */
++ (BOOL)cancelAuthWithPlatform:(JSHAREPlatform)platfrom;
+
+
+/**
+ 检查不存在新浪客户端情况下的网页端是否登陆
+ 
+ @return 返回结果
+ */
++ (BOOL)isSinaWeiboWebLogined;
+
+
+/**
+ 登出新浪网页端最新帐号
+ 
+ @return 返回结果
+ */
++ (BOOL)sinaWeiboWebLogOut;
+
+
+/**
+ 检查是否安装微信客户端
+ 
+ @return 返回结果
+ */
++ (BOOL)isWeChatInstalled;
+
+/**
+ 检查是否存在QQ客户端
+ 
+ @return 返回结果
+ */
++ (BOOL)isQQInstalled;
+
+
+/**
+ 检查是否存在新浪微博客户端
+ 
+ @return 返回结果
+ */
++ (BOOL)isSinaWeiBoInstalled;
+
+
+
+/**
+ @abstract 设置是否打印sdk产生的Debug级log信息, 默认为NO(不打印log)
+ 
+ SDK 默认开启的日志级别为: Info. 只显示必要的信息, 不打印调试日志.
+ 
+ 请在SDK启动后调用本接口，调用本接口可打开日志级别为: Debug, 打印调试日志.
+ 请在发布产品时改为NO，避免产生不必要的IO
+ 
+ 此接口必须在"setupWithConfig:"之后调用才会生效.
+ @param enable 是否打印
+ */
++ (void)setDebug:(BOOL)enable;
+
+
+@end
+
+
+/**
+ 社交平台授权信息。
+ */
+@interface JSHARESocial : NSObject
+
+/**
+ 对应新浪微博平台的uid，以及QQ与微信平台可能会存在的unionid。
+ */
+@property (nonatomic, copy) NSString  *uid;
+
+/**
+ 对应QQ与微信平台授权提供的openId。
+ */
+@property (nonatomic, copy) NSString  *openid;
+
+/**
+ accessToken 用户允许授权获取的 accessToken
+ */
+@property (nonatomic, copy) NSString  *accessToken;
+
+/**
+ accessToken有效期的时间戳，单位为秒。
+ */
+@property (nonatomic, assign) long expiration;
+
+/**
+ refreshToken 用来刷新 accessToken 的有效期。
+ */
+@property (nonatomic, copy) NSString  *refreshToken;
+
+
+/**
+ 对应的社交平台。
+ */
+@property (nonatomic, assign) JSHAREPlatform platform;
+
+/**
+ 社交平台提供的授权原始数据。
+ */
+@property (nonatomic,strong) NSDictionary *oauthOriginalResponse;
+
+@end
+
+
+@interface JSHARESocialUserInfo : JSHARESocial
+
+/**
+ 社交平台的用户昵称。
+ */
+@property (nonatomic, copy) NSString  *name;
+
+/**
+ 社交平台的用户头像链接。
+ */
+@property (nonatomic, copy) NSString  *iconurl;
+
+/**
+ 社交平台的用户性别，数值1表示男，数值2表示女。
+ */
+@property (nonatomic, assign) NSInteger gender;
+
+/**
+ 社交平台提供的用户信息原始数据。
+ */
+@property (nonatomic, strong) NSDictionary *userOriginalResponse;
+
+@end
+
---
+++ ios/RCTJShareModule/RCTJShareModule.m
@@ -0,0 +1,477 @@
+//
+//  RCTJShareModule.m
+//  jshare
+//
+//  Created by oshumini on 2017/7/18.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "RCTJShareModule.h"
+#import "JSHAREService.h"
+
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTBridge.h>
+#elif __has_include("RCTBridge.h")
+#import "RCTEventDispatcher.h"
+#import "RCTRootView.h"
+#import "RCTBridge.h"
+#elif __has_include("React/RCTBridge.h")
+#import "React/RCTEventDispatcher.h"
+#import "React/RCTRootView.h"
+#import "React/RCTBridge.h"
+#endif
+
+@implementation RCTJShareModule
+
+RCT_EXPORT_MODULE();
+
+//@synthesize bridge = _bridge;
+
++ (id)allocWithZone:(NSZone *)zone {
+  static RCTJShareModule *sharedInstance = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    sharedInstance = [super allocWithZone:zone];
+  });
+  return sharedInstance;
+}
+
+- (id)init {
+  self = [super init];
+  return self;
+}
+
+
+
+//
+///**
+// 启动SDK,必要!
+// 
+// @param config SDK启动参数模型，不可为nil。
+// */
+//+ (void)setupWithConfig:(JSHARELaunchConfig *)config;
+//
+//
+
+
+//JSHARELaunchConfig *config = [[JSHARELaunchConfig alloc] init];
+//config.appKey = @"a1703c14b186a68a66ef86c1";
+//config.SinaWeiboAppKey = @"374535501";
+//config.SinaWeiboAppSecret = @"baccd12c166f1df96736b51ffbf600a2";
+//config.SinaRedirectUri = @"https://www.jiguang.cn";
+//config.QQAppId = @"1105864531";
+//config.QQAppKey = @"glFYjkHQGSOCJHMC";
+//config.WeChatAppId = @"wxc40e16f3ba6ebabc";
+//config.WeChatAppSecret = @"dcad950cd0633a27e353477c4ec12e7a";
+//config.isSupportWebSina = YES;
+//[JSHAREService setupWithConfig:config];
+//[JSHAREService setDebug:YES];
+
+- (JSHAREPlatform)getPlatformFromDic:(NSDictionary *)param {
+  JSHAREPlatform platform = 0;
+  
+  if (param[@"platform"]) {
+    if ([param[@"platform"] isEqualToString:@"wechat_session"]) {
+      platform = JSHAREPlatformWechatSession;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"wechat_timeLine"]) {
+      platform = JSHAREPlatformWechatTimeLine;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"wechat_favourite"]) {
+      platform = JSHAREPlatformWechatFavourite;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"qq"]) {
+      platform = JSHAREPlatformQQ;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"qzone"]) {
+      platform = JSHAREPlatformQzone;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"sina_weibo"]) {
+      platform = JSHAREPlatformSinaWeibo;
+    }
+    
+    if ([param[@"platform"] isEqualToString:@"sina_weibo_contact"]) {
+      platform = JSHAREPlatformSinaWeiboContact;
+    }
+  }
+  
+  return platform;
+
+}
+
+RCT_EXPORT_METHOD(setup:(NSDictionary *)param){
+  JSHARELaunchConfig *config = [[JSHARELaunchConfig alloc] init];
+  if (param[@"appKey"]) {
+    config.appKey = param[@"appKey"];
+  }
+  
+  if (param[@"channel"]) {
+    config.channel = param[@"channel"];
+  }
+  
+  if (param[@"advertisingId"]) {
+    config.advertisingId = param[@"advertisingId"];
+  }
+  
+  if (param[@"isProduction"]) {
+    config.isProduction = param[@"isProduction"];
+  }
+  
+  if (param[@"wechatAppId"]) {
+    config.WeChatAppId = param[@"wechatAppId"];
+  }
+  
+  if (param[@"wechatAppSecret"]) {
+    config.WeChatAppSecret = param[@"wechatAppSecret"];
+  }
+  
+  if (param[@"qqAppId"]) {
+    config.QQAppId = param[@"qqAppId"];
+  }
+  
+  if (param[@"qqAppKey"]) {
+    config.QQAppKey = param[@"qqAppKey"];
+  }
+  
+  if (param[@"sinaWeiboAppKey"]) {
+    config.SinaWeiboAppKey = param[@"sinaWeiboAppKey"];
+  }
+  
+  if (param[@"sinaWeiboAppSecret"]) {
+    config.SinaWeiboAppSecret = param[@"sinaWeiboAppSecret"];
+  }
+  
+  if (param[@"sinaRedirectUri"]) {
+    config.SinaRedirectUri = param[@"sinaRedirectUri"];
+  }
+  
+  if (param[@"isSupportWebSina"]) {
+    NSNumber *isSupportWebSina = param[@"isSupportWebSina"];
+    config.isSupportWebSina = [isSupportWebSina boolValue];
+  }
+  
+  [JSHAREService setupWithConfig:config];
+}
+
+
+RCT_EXPORT_METHOD(getSoicalUserInfo:(NSDictionary *)param
+                  success:(RCTResponseSenderBlock) successCallBack
+                  fail:(RCTResponseSenderBlock) failCallBack) {
+
+  JSHAREPlatform platform = [self getPlatformFromDic:param];
+  
+  if (platform == 0) {
+    failCallBack(@[@{@"code": @(1), @"description": @"platform 参数错误"}]);
+    return;
+  }
+  
+  [JSHAREService getSoicalUserInfo:platform handler:^(JSHARESocialUserInfo *userInfo, NSError *error) {
+    NSMutableDictionary *userDic = @{}.mutableCopy;
+    if (error) {
+      NSString *descript = [error description];
+      failCallBack(@[@{@"code": @(1), @"description": [error description]}]);
+      return;
+    }
+    
+    userDic[@"name"] = userInfo.name;
+    userDic[@"iconUrl"] = userInfo.iconurl;
+    if (userInfo.gender == 1) {
+      userDic[@"gender"] = @"male";
+    } else {
+      userDic[@"gender"] = @"female";
+    }
+    userDic[@"response"] = userInfo.userOriginalResponse;
+    successCallBack(@[[NSDictionary dictionaryWithDictionary: userDic]]);
+  }];
+}
+
+RCT_EXPORT_METHOD(isPlatformAuth:(NSDictionary *)param
+                  success:(RCTResponseSenderBlock) successCallBack) {
+  JSHAREPlatform platform = [self getPlatformFromDic:param];
+  
+  if (platform == 0) {
+    return;
+  }
+  
+    BOOL result = [JSHAREService isPlatformAuth:platform];
+    successCallBack(@[@(result)]);
+}
+
+RCT_EXPORT_METHOD(cancelAuthWithPlatform:(NSDictionary *)param
+                  success:(RCTResponseSenderBlock) successCallBack) {
+  
+  JSHAREPlatform platform = [self getPlatformFromDic:param];
+  
+  if (platform == 0) {
+    successCallBack(@[@{@"code": @(1), @"description": @"platform 参数错误"}]);
+    return;
+  }
+  
+  BOOL result = [JSHAREService cancelAuthWithPlatform:platform];
+  successCallBack(@[@(result)]);
+}
+
+RCT_EXPORT_METHOD(isSinaWeiboWebLogined:(RCTResponseSenderBlock)successCallBack) {
+  BOOL result = [JSHAREService isSinaWeiboWebLogined];
+  successCallBack(@[@(result)]);
+}
+
+RCT_EXPORT_METHOD(sinaWeiboWebLogOut:(RCTResponseSenderBlock) successCallBack) {
+  BOOL result = [JSHAREService sinaWeiboWebLogOut];
+  successCallBack(@[@(result)]);
+}
+
+RCT_EXPORT_METHOD(isWeChatInstalled:(RCTResponseSenderBlock) successCallBack) {
+  BOOL result = [JSHAREService isWeChatInstalled];
+  successCallBack(@[@(result)]);
+}
+
+RCT_EXPORT_METHOD(isQQInstalled:(RCTResponseSenderBlock) successCallBack) {
+  BOOL result = [JSHAREService isQQInstalled];
+  successCallBack(@[@(result)]);
+}
+
+
+RCT_EXPORT_METHOD(isSinaWeiBoInstalled:(RCTResponseSenderBlock) successCallBack) {
+  BOOL result = [JSHAREService isSinaWeiBoInstalled];
+  successCallBack(@[@(result)]);
+}
+
+
+RCT_EXPORT_METHOD(setDebug:(NSDictionary *)param) {
+  NSNumber *enable = false;
+  if (param[@"enable"]) {
+    enable = param[@"enable"];
+  }
+  [JSHAREService setDebug:[enable boolValue]];
+}
+
+RCT_EXPORT_METHOD(share:(NSDictionary *)param
+                  success:(RCTResponseSenderBlock) successCallBack
+                  fail:(RCTResponseSenderBlock) failCallBack) {
+
+  if (param[@"type"] == nil) {
+    failCallBack(@[@{@"code": @(1), @"description": @"parame error"}]);
+    return;
+  }
+  
+  JSHAREMessage *message = [JSHAREMessage message];
+  JSHAREPlatform platform = [self getPlatformFromString: param[@"platform"]];
+  
+  if (platform) {
+    message.platform = platform;
+  } else {
+    failCallBack(@[@{@"code":@(1), @"description": @"parame error: platform error"}]);
+  }
+  
+  if ([param[@"type"] isEqualToString: @"text"]) {
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    message.mediaType = JSHAREText;
+
+  }
+  
+  if ([param[@"type"] isEqualToString: @"image"]) {
+    if (param[@"image"]) {
+      message.image = [NSData dataWithContentsOfFile:param[@"image"]];
+    }
+    
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    
+    if (param[@"imageArray"]) {
+      NSArray *imageArr = param[@"imageArray"];
+      NSMutableArray *imageDataArr = @[].mutableCopy;
+      for (NSString *path in imageArr) {
+        NSData *data = [NSData dataWithContentsOfFile: path];
+        [imageDataArr addObject:data];
+      }
+      message.images = imageDataArr;
+      
+    }
+    message.mediaType =JSHAREImage;
+  }
+  
+  if ([param[@"type"] isEqualToString: @"video"]) {
+    if (param[@"title"]) {
+      message.title = param[@"title"];
+    }
+    
+    if (param[@"url"]) {
+      message.mediaDataUrl = param[@"url"];
+    }
+    
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    
+    if (param[@"imagePath"]) {
+      message.thumbnail = [NSData dataWithContentsOfFile:param[@"imagePath"]];
+    }
+    
+    message.mediaType = JSHAREVideo;
+  }
+  
+  if ([param[@"type"] isEqualToString: @"audio"]) {
+    if (param[@"title"]) {
+      message.title = param[@"title"];
+    }
+    
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    
+    if (param[@"url"]) {
+      message.mediaDataUrl = param[@"url"];
+    }
+    
+    if (param[@"imagePath"]) {
+      message.thumbnail = [NSData dataWithContentsOfFile:param[@"imagePath"]];
+    }
+    
+    if (param[@"musicUrl"]) {
+      message.mediaDataUrl = param[@"musicUrl"];
+    }
+    
+    message.mediaType = JSHAREAudio;
+  }
+  
+  if ([param[@"type"] isEqualToString: @"file"]) {
+    message.mediaType = JSHAREFile;
+    if (param[@"path"]) {
+      message.fileData = [NSData dataWithContentsOfFile:param[@"path"]];
+    }
+    
+    if (param[@"title"]) {
+      message.title = param[@"title"];
+    }
+    
+    if (param[@"fileExt"]) {
+      message.fileExt = param[@"fileExt"];
+    }
+    
+  }
+  
+  if ([param[@"type"] isEqualToString: @"emoticon"]) {
+    if (param[@"imagePath"]) {
+      message.emoticonData = [NSData dataWithContentsOfFile:param[@"imagePath"]];
+    }
+    
+    message.mediaType = JSHAREEmoticon;
+  }
+  
+  if ([param[@"type"] isEqualToString: @"app"]) {
+    if (param[@"title"]) {
+      message.title = param[@"title"];
+    }
+    
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    
+    if (param[@"url"]) {
+      message.url = param[@"url"];
+    }
+    
+    if (param[@"extInfo"]) {
+      message.extInfo = param[@"extInfo"];
+    }
+    
+    if (param[@"path"]) {
+      message.fileData = [NSData dataWithContentsOfFile:param[@"path"]];
+    }
+    message.mediaType = JSHAREApp;
+  }
+  
+  if ([param[@"type"] isEqualToString: @"link"]) {
+    
+    if (param[@"title"]) {
+      message.title = param[@"title"];
+    }
+    
+    if (param[@"text"]) {
+      message.text = param[@"text"];
+    }
+    
+    if (param[@"url"]) {
+      message.url = param[@"url"];
+    }
+    
+    if (param[@"imagePath"]) {
+      message.thumbnail = [NSData dataWithContentsOfFile:param[@"imagePath"]];
+    }
+    message.mediaType = JSHARELink;
+  }
+  
+  [JSHAREService share:message handler:^(JSHAREState state, NSError *error) {
+    if (error) {
+      failCallBack(@[@{@"code":@(error.code), @"description": [error description]}]);
+    }
+    NSString *stateString = [self stateToString:state];
+    successCallBack(@[@{@"state": stateString}]);
+  }];
+}
+
+- (NSUInteger)getPlatformFromString:(NSString *)platformStr {
+  if ([platformStr isEqualToString:@"wechat_session"]) {
+    return JSHAREPlatformWechatSession;
+  }
+  
+  if ([platformStr isEqualToString:@"wechat_timeLine"]) {
+    return JSHAREPlatformWechatTimeLine;
+  }
+  
+  if ([platformStr isEqualToString:@"wechat_favourite"]) {
+    return JSHAREPlatformWechatFavourite;
+  }
+  
+  if ([platformStr isEqualToString:@"qq"]) {
+    return JSHAREPlatformQQ;
+  }
+  
+  if ([platformStr isEqualToString:@"qzone"]) {
+    return JSHAREPlatformQzone;
+  }
+  
+  if ([platformStr isEqualToString:@"sina_weibo"]) {
+    return JSHAREPlatformSinaWeibo;
+  }
+  
+  if ([platformStr isEqualToString:@"sina_weibo_contact"]) {
+    return JSHAREPlatformSinaWeiboContact;
+  }
+  
+  return 0;
+}
+
+- (NSString *)stateToString:(JSHAREState)state {
+  NSString *stateString = @"unknow";
+  switch (state) {
+    case JSHAREStateSuccess:
+      stateString = @"success";
+      break;
+      
+    case JSHAREStateFail:
+      stateString = @"fail";
+      break;
+      
+    case JSHAREStateCancel:
+      stateString = @"cancel";
+      break;
+      
+    case JSHAREStateUnknown:
+      stateString = @"unknown";
+      break;
+  }
+  return stateString;
+}
+@end
