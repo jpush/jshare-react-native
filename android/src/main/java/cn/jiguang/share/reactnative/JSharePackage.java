bd3e548c10694a4258e28766f4957c7aff880ac2
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2017-10-26 14:18:27 +0800

    support rn ^0.47.1

--- android/src/main/java/cn/jiguang/share/reactnative/JSharePackage.java
+++ android/src/main/java/cn/jiguang/share/reactnative/JSharePackage.java
@@ -24,7 +24,6 @@ public class JSharePackage implements ReactPackage {
         });
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
