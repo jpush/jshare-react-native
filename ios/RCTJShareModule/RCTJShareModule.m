116c020f412284fb343366f7583fcadce3299973
huangminlinux <380108184@qq.com>
2018-01-19 15:52:20 +0800

    fix share message crash bug

--- ios/RCTJShareModule/RCTJShareModule.m
+++ ios/RCTJShareModule/RCTJShareModule.m
@@ -405,14 +405,16 @@ RCT_EXPORT_METHOD(share:(NSDictionary *)param
     message.mediaType = JSHARELink;
   }
 
-  [JSHAREService share:message handler:^(JSHAREState state, NSError *error) {
-    if (error) {
-      failCallBack(@[@{@"code":@(error.code), @"description": [error description]}]);
-      return;
-    }
-    NSString *stateString = [self stateToString:state];
-    successCallBack(@[@{@"state": stateString}]);
-  }];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [JSHAREService share:message handler:^(JSHAREState state, NSError *error) {
+      if (error) {
+        failCallBack(@[@{@"code":@(error.code), @"description": [error description]}]);
+        return;
+      }
+      NSString *stateString = [self stateToString:state];
+      successCallBack(@[@{@"state": stateString}]);
+    }];
+  });
 }
 
 - (NSUInteger)getPlatformFromString:(NSString *)platformStr {
